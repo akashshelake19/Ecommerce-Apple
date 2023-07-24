50feb39c7758e24bb41b3ac0512f64470e01e1dd
akash <am.shelake19@gmail.com>
2023-07-25 01:32:30 +0530

    allowed host enabled

--- Apple/settings.py
+++ Apple/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-6^^(lw62a4=8r=u-(%x0@rhyukk*-7e$8wnz8sgip&ks@jc7=9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
